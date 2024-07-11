2c22596d2c2a8616248c4621e1edb224c9924f54
Artyom Egorov <egorov.a@cpateam.xyz>
2024-07-12 02:18:55 +0300

    add

--- 11.09/tasks/script.js
+++ 11.09/tasks/script.js
@@ -15,3 +15,78 @@
 // }
 
 //task bk 2
+//while
+// let n = 10;
+// let sum = 0;
+
+// while (n > 0) {
+// 	sum += n;
+// 	n -= 1;
+// }
+
+// console.log("Result " + sum);
+
+//task bk 3
+//do while
+// let m = 10;
+// const n = 20;
+// let result = 1;
+
+// do {
+// 	if (m % 2 === 1) {
+// 		result *= m;
+// 	}
+//     m++
+// } while (m <= n);
+
+// console.log(result);
+
+//task bk 4
+//for
+
+// let result = 1;
+
+// for (let m = 10; m <= 20; m++) {
+// 	if (m % 2 === 1) {
+// 		result *= m;
+// 	}
+// }
+
+// console.log(result);
+
+//task bk 5
+//for calculate
+
+// for(let i = 1; i <= 10; i++) {
+//     console.log('\n=>' + i + '<=')
+
+//     for(let j = 1; j <= 10; j++) {
+//         console.log(i + 'x' + j + '=' + i * j)
+//     }
+// }
+
+//task bk 6
+//while calculate prompt +
+
+// let result = 0;
+
+// while (true) {
+// 	const userInput = prompt("You number ?", "");
+// 	if (!userInput) {
+// 		break;
+// 	}
+// 	result += Number(userInput);
+// }
+// console.log(result);
+
+//task bk 7
+//minification
+
+let result = 1;
+
+for (let m = 10; m <= 20; m++) {
+	if (m % 2 === 0) continue;
+	result += m;
+}
+
+console.log(result);
